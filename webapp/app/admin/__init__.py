1d5826e0655cadb91338305b67651c5a9bb46ae9
kuromaster <shibanov.ai@mail.ru>
2021-03-11 16:30:46 +0300

    remove all files

--- webapp/app/admin/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-
-admin_panel = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
--- webapp/start.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from app import db
-from app import view
-from app.admin import admin_panel, view_admin
-
-app.register_blueprint(admin_panel, url_prefix='/admin')
-
-if __name__ == "__main__":
-    app.run(threaded=True, debug=True)
